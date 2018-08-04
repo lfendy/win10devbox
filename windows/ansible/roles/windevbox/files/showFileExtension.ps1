23d5300396172a0735facbbfad7146cf336df499
Liauw Fendy <lfendy@gmail.com>
2018-08-04 16:15:23 +0800

    many changes to install vs2017

---
+++ windows/ansible/roles/windevbox/files/showFileExtension.ps1
@@ -0,0 +1,12 @@
+function ShowFileExtensions()
+{
+    Push-Location
+    Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
+    Set-ItemProperty . HideFileExt "0"
+    Pop-Location
+}
+
+function RefreshExplorer()
+{
+    Stop-Process -processName: Explorer -force
+}
